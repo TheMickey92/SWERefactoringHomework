0dd2347906fa56470616f72cd2e7a3c117db2b8a
TheMotivation <marcel.braeuninger@gmail.com>
2014-05-15 14:51:08 +0200

    Replaced conditonals in Price getCharge() with Polymorphism

--- ChildrensPrice.java
+++ ChildrensPrice.java
@@ -12,9 +12,19 @@ public class ChildrensPrice extends Price
 {
 
     @Override
-    int getPriceCode()
+    public int getPriceCode()
     {
         return Movie.CHILDRENS;
     }
-    
+
+    @Override
+    public double getCharge(int daysRented)
+    {
+        double result = 1.5;
+        if (daysRented > 3)
+        {
+            result += (daysRented - 3) * 1.5;
+        }
+        return result;
+    }
 }
--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -11,9 +11,15 @@ package swerefactoringhomework;
 public class NewReleasePrice extends Price
 {
     @Override
-    int getPriceCode()
+    public int getPriceCode()
     {
         return Movie.NEW_RELEASE;
     }
     
+    @Override
+    public double getCharge(int daysRented)
+    {
+        double result = daysRented * 3;
+        return result;
+    }
 }
